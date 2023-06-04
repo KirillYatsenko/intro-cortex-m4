ffe9bf44bbf8329439ccbe8de891e15628f76986
Kirill Yatsenko <kirill.yatsenko@camlingroup.com>
2023-06-04 12:32:53 +0200

    examples: st7735r: initial

---
+++ drivers/st7735r.h
@@ -0,0 +1,13 @@
+#ifndef _ST7735R_H_
+#define _ST7735R_H_
+
+/*  Wiring
+ * PA7 - D/C
+ * PA6 - RST
+ * PB5 - SCE
+ * PB4 - SCLK
+ * PB7 - MOSI
+ */
+void st7735r_init(void);
+
+#endif
---
+++ examples/st7735r/main.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "st7735r.h"
+
+int main(void)
+{
+	st7735r_init();
+
+	while (true) { }
+}
