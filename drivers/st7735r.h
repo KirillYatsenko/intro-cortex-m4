4819f0c8ca6e038e6a7cd78fec5895282f78191a
Kirill Yatsenko <kirill.yatsenko@camlingroup.com>
2023-06-05 19:56:05 +0200

    examples: st7735r: draw example.bmp

--- drivers/st7735r.h
+++ drivers/st7735r.h
@@ -1,6 +1,8 @@
 #ifndef _ST7735R_H_
 #define _ST7735R_H_
 
+#include <stdint.h>
+
 /*  Wiring
  * PA7 - D/C
  * PA6 - RST
@@ -8,6 +10,11 @@
  * PB4 - SCLK
  * PB7 - MOSI
  */
-void st7735r_init(void);
+void st7735r_init();
+void st7735r_clear_display();
+void st7735r_set_color(uint16_t red, uint16_t green, uint16_t blue);
+void st7735r_draw_rectangle(uint8_t x1, uint8_t x2, uint8_t y1, uint8_t y2);
+void st7735r_draw_bitmap(int16_t x, int16_t y, const uint16_t *image,
+			 int16_t width, int16_t height);
 
 #endif
--- examples/st7735r/main.c
+++ examples/st7735r/main.c
@@ -1,11 +1,15 @@
 #include <stdint.h>
 #include <stdbool.h>
-
 #include "st7735r.h"
+
+#include "example_bmp.h"
 
 int main(void)
 {
 	st7735r_init();
+
+	st7735r_clear_display();
+	st7735r_draw_bitmap(0, 127, image, 160, 128);
 
 	while (true) { }
 }
