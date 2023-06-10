0a1d71d0147646530c50fa77c6c07a3a54086807
Kirill Yatsenko <kirill.yatsenko@camlingroup.com>
2023-06-10 15:01:13 +0200

    tools: bmpconvert: upgrade

--- tools/bmpconvert.c
+++ tools/bmpconvert.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <ctype.h>
 
 #define PIXEL_ARRAY_OFFSET		0x0A
 #define WIDTH_OFFSET			0x12
@@ -51,65 +52,91 @@ static int exit_error(FILE *file)
 	exit(-1);
 }
 
+static char *strtoup(char *src, char *dst)
+{
+	char *dst_start = dst;
+
+	while (*src)
+		*(dst++) = toupper(*(src++));
+
+	*dst = '\0';
+
+	return dst_start;
+}
+
 // Todo: print error message based on errno
 int main(int argc, char *argv[])
 {
 	int ret;
-	FILE *file_in, *file_out;
-	char *file_in_basename;
+	FILE *file;
+	char *file_name, *file_ext;
 	uint16_t pixel;
 	uint32_t bitmap_offset, width, height, i;
+	char file_name_up[1024];
 
 	if (argc != 2 || !strncmp(argv[1], "--help", 6)) {
 		print_usage();
 		return -1;
 	}
 
-	file_in = fopen(argv[1], "r");
-	if (!file_in) {
+	file = fopen(argv[1], "r");
+	if (!file) {
 		printf("Unable to open provided file: '%s'\n", argv[1]);
 		exit_error(NULL);
 	}
 
-	ret = read_header_entry(file_in, WIDTH_OFFSET, &width, sizeof(width));
+	file_name = basename(argv[1]);
+	file_ext = strstr(file_name, ".bmp");
+	if (!file_ext) {
+		printf("File format is not recognized, should be: .bmp\n");
+		exit_error(file);
+	}
+
+	*file_ext = '\0'; // trim extension
+
+	ret = read_header_entry(file, WIDTH_OFFSET, &width, sizeof(width));
 	if (ret) {
 		printf("Unable to read width size\n");
-		exit_error(file_in);
+		exit_error(file);
 	}
 
-	ret = read_header_entry(file_in, HEIGHT_OFFSET, &height, sizeof(height));
+	ret = read_header_entry(file, HEIGHT_OFFSET, &height, sizeof(height));
 	if (ret) {
 		printf("Unable to read height size\n");
-		exit_error(file_in);
+		exit_error(file);
 	}
 
-	ret = read_header_entry(file_in, PIXEL_ARRAY_OFFSET, &bitmap_offset,
+	ret = read_header_entry(file, PIXEL_ARRAY_OFFSET, &bitmap_offset,
 							sizeof(bitmap_offset));
 	if (ret) {
 		printf("Unable to read pixel array offset\n");
-		exit_error(file_in);
+		exit_error(file);
 	}
 
-	ret = fseek(file_in, bitmap_offset, SEEK_SET);
+	ret = fseek(file, bitmap_offset, SEEK_SET);
 	if (ret) {
 		printf("Unable to call fseek to bitmap data\n");
-		exit_error(file_in);
+		exit_error(file);
 	}
 
-	printf("Image width: %dpx\n", width);
-	printf("Image height: %dpx\n\n", height);
+	printf("#ifndef _%s_H_\n", strtoup(file_name, file_name_up));
+	printf("#define _%s_H_\n\n", file_name_up);
+	printf("#include <stdint.h>\n\n");
 
-	printf("const uint16_t image[] = {");
+	printf("const uint16_t %s_width = %d;\n", file_name, width);
+	printf("const uint16_t %s_height = %d;\n\n", file_name, height);
+
+	printf("const uint16_t %s[] = {", file_name);
 
 	for (i = 0; i < width * height; i++) {
 		// if first in the row
 		if (i % BYTES_PER_LINE == 0)
 			printf("\n\t");
 
-		ret = fread(&pixel, BYTES_PER_PIXEL, 1, file_in);
+		ret = fread(&pixel, BYTES_PER_PIXEL, 1, file);
 		if (ret != 1) {
 			printf("Unable to read pixel: #%d\n", i);
-			exit_error(file_in);
+			exit_error(file);
 		}
 
 		printf("0x%.4x,", pixel);
@@ -119,9 +146,10 @@ int main(int argc, char *argv[])
 			printf(" ");
 	}
 
-	printf("\n};\n");
+	printf("\n};\n\n");
+	printf("#endif\n");
 
-	fclose(file_in);
+	fclose(file);
 
 	return ret;
 }
